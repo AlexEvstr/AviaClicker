b5a3dd9b8d540d96c8b0113d58a378a08609efc6
Alexander Evstratov <alexdrugev@me.com>
2024-02-09 14:07:36 +0300

    Add plane sprite saver.

--- Assets/_Scripts/PlaneRotation.cs
+++ Assets/_Scripts/PlaneRotation.cs
@@ -6,11 +6,43 @@ using UnityEngine.EventSystems;
 
 public class PlaneRotation : MonoBehaviour, IPointerDownHandler, IPointerUpHandler
 {
+    [SerializeField] private Sprite[] _planes;
+ 
     private float _rotationIndex = 0.05f;
 
     private void Start()
     {
+        LoadSprite();
         StartCoroutine(ChangeRotationIndex());
+    }
+
+    private void LoadSprite()
+    {
+        int planeSprite = PlayerPrefs.GetInt("planeSprite", 0);
+        if (planeSprite == 0)
+        {
+            gameObject.GetComponent<Image>().sprite = _planes[0];
+        }
+        else if (planeSprite == 1)
+        {
+            gameObject.GetComponent<Image>().sprite = _planes[1];
+        }
+        else if (planeSprite == 2)
+        {
+            gameObject.GetComponent<Image>().sprite = _planes[2];
+        }
+        else if (planeSprite == 3)
+        {
+            gameObject.GetComponent<Image>().sprite = _planes[3];
+        }
+        else if (planeSprite == 4)
+        {
+            gameObject.GetComponent<Image>().sprite = _planes[4];
+        }
+        else if (planeSprite == 5)
+        {
+            gameObject.GetComponent<Image>().sprite = _planes[5];
+        }
     }
 
     private IEnumerator ChangeRotationIndex()
@@ -39,6 +71,35 @@ public class PlaneRotation : MonoBehaviour, IPointerDownHandler, IPointerUpHandl
 
     private void OnDisable()
     {
-        //gameObject.GetComponent<Image>().sprite
+        SaveSprite();
+    }
+
+    private void SaveSprite()
+    {
+        Debug.Log(gameObject.GetComponent<Image>().sprite.name);
+        if (gameObject.GetComponent<Image>().sprite.name.Contains("0"))
+        {
+            PlayerPrefs.SetInt("planeSprite", 0);
+        }
+        else if (gameObject.GetComponent<Image>().sprite.name.Contains("1"))
+        {
+            PlayerPrefs.SetInt("planeSprite", 1);
+        }
+        else if (gameObject.GetComponent<Image>().sprite.name.Contains("2"))
+        {
+            PlayerPrefs.SetInt("planeSprite", 2);
+        }
+        else if (gameObject.GetComponent<Image>().sprite.name.Contains("3"))
+        {
+            PlayerPrefs.SetInt("planeSprite", 3);
+        }
+        else if (gameObject.GetComponent<Image>().sprite.name.Contains("4"))
+        {
+            PlayerPrefs.SetInt("planeSprite", 4);
+        }
+        else if (gameObject.GetComponent<Image>().sprite.name.Contains("5"))
+        {
+            PlayerPrefs.SetInt("planeSprite", 5);
+        }
     }
 }
